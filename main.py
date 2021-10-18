b0fc752200834074dfd8d97a6e531b2068d41663
Bruno Grifo <leitaogrifo@gmail.com>
2021-10-18 18:09:34 +0000

    fact and compliment apis working fine

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import os
+import json
 import discord
+import requests
+from jokepy import Jokepy
 
 client = discord.Client()
+
+def get_joke():
+	j = Jokepy(categories=['Dark'])  # Initialise the class
+	return j.get_joke()
+
+def get_compliment():
+	response = requests.get('https://complimentr.com/api')
+	json_data = json.loads(response.text)
+	return json_data['compliment']
+
+def get_fact():
+	response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+	json_data = json.loads(response.text)
+	return json_data['text']
 
 @client.event
 async def on_ready():
@@ -11,9 +28,13 @@ async def on_ready():
 async def on_message(message):
 	if message.author == client:
 		return
+
+	if message.content.startswith("/hello"):
+		await message.channel.send('Hello {}\n{}'.format(message.author.name, get_compliment()))
 	
 	if message.content.startswith("/guru"):
-		await message.channel.send("Hello there")
+		await message.channel.send(get_fact())
+
 
 
 client.run(os.environ['TOKEN'])
