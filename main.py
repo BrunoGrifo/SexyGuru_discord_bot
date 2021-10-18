007bcc8eedf4bff0127ecfac7170e2930f24fcde
Bruno Grifo <leitaogrifo@gmail.com>
2021-10-18 16:52:35 +0000

    client bot created and ready fo dev

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import os
 import discord
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	print("We are live as {}".format(client))
+	print("We are live as {}".format(client.user))
 
 @client.event
 async def on_message(message):
 	if message.author == client:
 		return
 	
-	if message.content.startwith("/guru"):
+	if message.content.startswith("/guru"):
 		await message.channel.send("Hello there")
+
+
+client.run(os.environ['TOKEN'])
