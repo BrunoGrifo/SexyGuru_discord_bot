0efd02a8b2cb8efed572455b86a945e8f4348519
Bruno Grifo <leitaogrifo@gmail.com>
2021-10-22 10:59:25 +0000

    bad_words done

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
 import json
+import random
 import discord
 import requests
 from jokepy import Jokepy
 
 client = discord.Client()
+
+bad_words = ["fdx", "foda-se", "crlh", "caralho", "puta", "merda", "putedo", "cona", "paneleiro", "foder", "porra", "cabrão", "cabrao", "badalhoco", "badalhoca", "anormal", "rabeta", "xupa", "xupa-mos", "porca"]
+
+bad_words_lecture = [
+"Fala bem que já tens dentes!", 
+"Reparei que disseste uma palavra feia...voltas a dizer uma dessas e levas um rotativo nos olhos!",
+"Palavras feias não são bem vindas no Reino do Grifo",
+"Cristão não fala palavrão!",
+"Xô Satanás! Vai dizer palavrões para longe!",
+"Pai nosso que estás no céu perdoai este pecador pois ele não sabe falai em condições e livrai-lhe de todos os palavrões, amén."
+]
 
 def get_joke():
 	j = Jokepy(categories=['Dark'])  # Initialise the class
@@ -34,6 +46,13 @@ async def on_message(message):
 	
 	if message.content.startswith("/guru"):
 		await message.channel.send(get_fact())
+
+	if any(word.lower() in message.content for word in bad_words):
+		await message.channel.send(random.choice(bad_words_lecture))
+
+
+
+
 
 
 
